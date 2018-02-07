517d1fd672e6a03f1553fbdeeda8461ed7399178
Shaamala <shaamalajabbar@gmail.com>
2018-02-07 11:46:50 -0500

    add a new line to the file

--- src/MergePractice.java
+++ src/MergePractice.java
@@ -6,6 +6,8 @@ public class MergePractice {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("My name is Nick");
+		
+		System.out.println(" This is Abdul's addition to the file");
 
 	}
 
