c9f9bd9df81c0e75fca35d3113b5b5eff2e287c0
nicksoule <nicksoule@live.com>
2018-02-07 11:31:00 -0500

    initial commit

---
+++ src/MergePractice.java
@@ -0,0 +1,12 @@
+/*
+ * @ author Nick Soule
+ */
+public class MergePractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Nick");
+
+	}
+
+}
